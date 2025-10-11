36584d2078c7bfd9e807c6c81d3376a856eab803
carvex21 <anas.valeras@gmail.com>
2025-10-11 21:18:30 +0200

    ~~ add DoctorDtoValidator and PatientDtoValidator

---
+++ SmartClinic.Core/Validators/DoctorDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using SmartClinic.Core.DTOs;
+
+namespace SmartClinic.Core.Validators;
+
+public class DoctorDtoValidator : AbstractValidator<DoctorDto>
+{
+    public DoctorDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Doctor ID must be greater than 0");
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .WithMessage("Doctor name cannot be empty")
+            .MaximumLength(100)
+            .WithMessage("Doctor name cannot exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .WithMessage("Doctor name cannot be empty")
+            .MaximumLength(100)
+            .WithMessage("Doctor name cannot exceed 100 characters");
+
+        RuleFor(x => x.Specialty)
+            .NotEmpty()
+            .WithMessage("Specialization cannot be empty")
+            .MaximumLength(100)
+            .WithMessage("Specialization cannot exceed 100 characters");
+    }
+}
---
+++ SmartClinic.Core/Validators/PatientDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using SmartClinic.Core.DTOs;
+
+namespace SmartClinic.Core.Validators;
+
+public class PatientDtoValidator : AbstractValidator<PatientDto>
+{
+    public PatientDtoValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required")
+            .MaximumLength(100)
+            .WithMessage("First name cannot exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .WithMessage("Last name is required")
+            .MaximumLength(100)
+            .WithMessage("Last name cannot exceed 100 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required")
+            .EmailAddress()
+            .WithMessage("A valid email address is required")
+            .MaximumLength(255)
+            .WithMessage("Email cannot exceed 255 characters");
+
+        RuleFor(x => x.Id)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("ID must be greater than or equal to 0");
+    }
+}
